4c63135427e2c6bdbe1dbac1d4b889e5b1c46d4a
TarunBaddi <158991051+TarunBaddi@users.noreply.github.com>
2024-03-24 22:09:34 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,32 @@
+package CIE_1.J3;
+
+// Employee.java
+public class Employee {
+    private String name;
+    private int employeeId;
+    private double salary;
+
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Salary: " + salary);
+    }
+}
